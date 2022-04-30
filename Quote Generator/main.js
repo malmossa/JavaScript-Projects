cc4e500b9b0352402e2180019155b00d5be56843
Mansor Almossa <malmossa@gmail.com>
2022-04-30 12:30:32 -0700

    Completed

--- Quote Generator/main.js
+++ Quote Generator/main.js
@@ -1,22 +1,26 @@
-const quotes = [
-  "The best and most beautiful things in the world cannot be seen or even touched - they must be felt with the heart.",
-  "Keep your face always toward the sunshine - and shadows will fall behind you.",
-  "Be kind, for whenever kindness becomes part of something, it beautifies it. Whenever it is taken from something, it leaves it tarnished.",
-  "The best preparation for tomorrow is doing your best today.",
-  "The greatest of richness is the richness of the soul.",
-  "The glow of one warm thought is to me worth more than money.",
-  "My mission in life is not merely to survive, but to thrive; and to do so with some passion, some compassion, some humor, and some style.",
-  "It is in your moments of decision that your destiny is shaped.",
-  "Happiness is not something you postpone for the future; it is something you design for the present.",
-  "Memories of our lives, of our works and our deeds will continue in others.",
-];
+let quotesArray = [];
 
 const quoteText = document.querySelector(".quote");
 const button = document.querySelector(".btn");
 
-button.addEventListener("click", displayQuote);
+button.addEventListener("click", newQuote);
 
-function displayQuote() {
-  const randomIndex = Math.floor(Math.random() * 9);
-  quoteText.innerText = quotes[randomIndex];
+// Get Quote from api
+async function getQuotes() {
+  const apiURL = "https://type.fit/api/quotes";
+
+  try {
+    const result = await fetch(apiURL);
+    quotesArray = await result.json();
+  } catch (err) {
+    console.error(err);
+  }
 }
+
+function newQuote() {
+  const randomIndex = Math.floor(Math.random() * quotesArray.length);
+  const quote = quotesArray[randomIndex];
+  quoteText.textContent = quote.text;
+}
+// On load
+getQuotes();
